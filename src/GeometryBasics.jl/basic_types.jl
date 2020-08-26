8618bc811dcf2584113e091bd4d02e0b8dee2ab6
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2020-08-26 13:59:13 +0900

    update

---
+++ src/GeometryBasics.jl/basic_types.jl
@@ -0,0 +1,43 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .jl
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.5.2
+#   kernelspec:
+#     display_name: Julia 1.5.0-rc2
+#     language: julia
+#     name: julia-1.5
+# ---
+
+using StaticArrays
+
+"""
+AbstractGeometry in R{Dim} with Number type T <: Number
+"""
+abstract type AbstractGeometry{Dim, T <: Number} end
+abstract type GeometryPrimitive{Dim, T} <: AbstractGeometry{Dim, T} end
+Base.ndims(x::AbstractGeometry{Dim}) where Dim = Dim
+
+"""
+Geometry made of N connected points. Connected as one flat geometry, it makes a Ngon/Polygon.
+Connected as volume it will be a Simplex/Tri/Cube.
+Note That `Polytope{N} where N == 3` denotes a Triangle both as a Simplex or Ngon.
+"""
+abstract type Polytope{Dim, T} <: AbstractGeometry{Dim, T} end
+abstract type AbstractPolygon{Dim, T} <: Polytope{Dim, T} end
+
+"""
+Point and Face as a subtype of StaticVector
+"""
+abstract type AbstractPoint{Dim ,T} <: StaticVector{Dim, T} end
+abstract type AbstractFace{N, T} <: StaticVector{N, T} end
+abstract type AbstractSimplex{Dim, N, T} <: StaticVector{Dim, T} end
+abstract type AbstractSimplexFace{N, T} <: AbstractFace{N, T} end
+abstract type AbstractNgonFace{N, T} <: AbstractFace{N, T} end
+
+"""
+Face index, connecting points to form a simplex
+"""
